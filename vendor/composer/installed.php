8c532decee8685abcd77df75ecc0f73af8897808
rifqihaidar <22450819+i33z@users.noreply.github.com>
2022-06-08 23:19:07 +0000

    test

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '4c78bb3f204d72459437f8aa34f555779faf54fc',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '4c78bb3f204d72459437f8aa34f555779faf54fc',
+    ),
+  ),
+);
